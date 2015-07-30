bd88b73be6d004715dde43dbbe0fbdde19bc1d59
michaelthomaswilson <mwilson@pure-brand.com>
2015-07-30 07:48:04 -0600

    fixed hero height issue..finally

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    headerHeight = $("#masthead").height();
+    bannerHeight = $(".copyBanner").height();
+    windowHeight = windowHeight-headerHeight;
+    //heroHeight = windowHeight - bannerHeight;
+    $('.fullscreen').css('height', windowHeight);
+    //$('.heroPhoto').css('height', heroHeight);
+  };
+  setHeight();
+  
+  $(window).resize(function() {
+    setHeight();
+  });
+});
